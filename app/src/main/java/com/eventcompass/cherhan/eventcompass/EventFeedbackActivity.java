b4b31d187cf17166c29dd417dda74ccecaa4b848
Nik <loikopodom@gmail.com>
2014-09-18 10:19:05 +0800

    Feedback page finished

--- app/src/main/java/com/eventcompass/cherhan/eventcompass/EventFeedbackActivity.java
+++ app/src/main/java/com/eventcompass/cherhan/eventcompass/EventFeedbackActivity.java
@@ -1,9 +1,10 @@
 package com.eventcompass.cherhan.eventcompass;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class EventFeedbackActivity extends Activity {
 
@@ -11,25 +12,15 @@ public class EventFeedbackActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_feedback);
-    }
 
+        final Button btnSubmit = (Button)findViewById(R.id.button);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.event_feedback, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent isubmit = new Intent(EventFeedbackActivity.this, EventFeedbackSent.class);
+                startActivity(isubmit);
+            }
+        });
     }
 }
